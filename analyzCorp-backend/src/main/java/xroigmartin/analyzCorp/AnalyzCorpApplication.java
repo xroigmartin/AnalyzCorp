c9554cd34d0583cc567058e118165d6ce448f763
Xavier Roig <xroigmartin@gmail.com>
2024-07-23 07:02:39 +0200

    create base project

---
+++ analyzCorp-backend/src/main/java/xroigmartin/analyzCorp/AnalyzCorpApplication.java
@@ -0,0 +1,13 @@
+package xroigmartin.analyzCorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzCorpApplication.class, args);
+	}
+
+}
---
+++ analyzCorp-backend/src/test/java/xroigmartin/analyzCorp/AnalyzCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package xroigmartin.analyzCorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
